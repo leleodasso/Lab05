1b0702e513d94e6cf344a4412a8c19644b7da43c
leleodasso <leleodasso@gmail.com>
2025-03-25 21:00:56 +0100

    completato punto 2 e 3

--- UI/controller.py
+++ UI/controller.py
@@ -18,12 +18,27 @@ class Controller:
     def handle_cercaIscritti(self, e):
         """Simple function to handle a button-pressed event,
         and consequently print a message on screen"""
-        nomeCorso = self._view.dd_studenti.value
+        nomeCorso = self._view.dd_corso.value
         if nomeCorso is None or nomeCorso == "":
             self._view.create_alert("Selezionare un corso!")
             return
-        self._view.txt_result.controls.append(ft.Text(f"hai selezionato il corso di, {nomeCorso}!"))
+        self._view.txt_result.controls.append(ft.Text(f"hai selezionato il corso di {nomeCorso}!"))
         self._view.update_page()
+
+        codiceCorso = daoC.getCodCorso(nomeCorso)
+        print(codiceCorso[0])
+        elencoMatricoleCorso = daoC.getElencoMatricoleCorso(codiceCorso[0])
+        for el in elencoMatricoleCorso:
+            print(el[0])
+        for el in elencoMatricoleCorso:
+            studente=daoC.getMatricolaNomeStudente(el[0])
+            strStudente = f"{studente[0][0]}, {studente[0][1]} ({studente[0][2]})"
+            self._view.txt_result.controls.append(ft.Text(f"{strStudente}"))
+        self._view.update_page()
+
+
+
+
 
     def handle_NomeCognome(self,e):
         # Creiamo un'istanza
--- database/corso_DAO.py
+++ database/corso_DAO.py
@@ -21,6 +21,51 @@ class CorsoDao:
         cnx.close()
         return nomiCorsi
 
+    def getCodCorso(self, nomeCorso):
+        cnx = get_connection()
+        cursor = cnx.cursor(buffered=True)
+
+        query = """SELECT codins FROM corso WHERE nome = %s"""
+        cursor.execute(query, (nomeCorso,))
+
+        result = cursor.fetchone()
+
+        cursor.close()
+        cnx.close()
+
+        return result
+
+    def getElencoMatricoleCorso(self, codCorso):
+        cnx = get_connection()
+        cursor = cnx.cursor()
+
+        query = """SELECT matricola FROM iscrizione WHERE codins = %s"""
+        cursor.execute(query, (codCorso,))
+
+        result = cursor.fetchall()
+
+        cursor.close()
+        cnx.close()
+
+        return result
+
+    def getMatricolaNomeStudente(self, matric):
+        cnx = get_connection()
+        cursor = cnx.cursor()
+
+        query = """SELECT nome, cognome, matricola FROM studente WHERE matricola = %s"""
+        cursor.execute(query, (matric,))
+
+        result = cursor.fetchall()
+
+        cursor.close()
+        cnx.close()
+
+        return result
+
+
+
+
 
 
 
